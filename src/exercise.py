ff569000b1dd5b08ea0ad37f449ebbd3e87ca714
Justin Turner <jmturner638@gmail.com>
2021-08-05 06:28:39 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,19 @@
+from item import Item
+
 def main():
     #write your code below this line
+    items = []
+
+    while True:
+        name = input()
+
+        if not name:
+            break
+
+        items.append(Item(name))
+
+    for item in items:
+        print(item)
 
 if __name__ == '__main__':
     main()
